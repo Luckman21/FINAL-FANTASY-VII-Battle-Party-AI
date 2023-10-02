7cb16ade8f4df16d1567667ca7db4a17a37460a3
Luckman21 <luckmaan21@gmail.com>
2023-10-01 20:43:44 -0400

    Basic Interface Commands
    This commit focuses on fleshing out the basic interface commands via keyboard input.  After importing keyboard, I am able to monitor keyboard inputs.  This allows me to create a "kill switch" for the program, which will later develop into a toggle.
    
    I was also able to create command macros.  These will be the bread and butter for the interface, as the AI model will call upon these functions to make decisions.  The macros remove the hard work of getting the AI to figure out how to navigate the menu.  This lets me tune the AI to a more battle strategy focus.

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Python Auto Typer
- This is a small test directory to play around with different features that will be implemented for a larger AI project.
+Luckman21
+
+FINAL FANTASY VII AI Battles
+
+The purpose of this project is for fun and learning.  My goal is to make a functional AI that can make the right decisions during
+battles and (hopefully) complete a full run of the game.  This project implements the following skills I'll need to pull this off:
+
+- Keyboard input generation
+- Keyboard key monitoring
+- Text from image processing
+- Interface for the AI to interact with the game
+- AI model to learn and make decisions
+
+Keyboard input generation will allow me to send commands to the game (as I'm playing on PC) like Attack, Magic, Summon, etc.
+
+By monitoring keyboard inputs, I can pause / start the battle algorithm and typing functions.  This way keys and battle functions 
+won't be spammed during battle.  
+
+Text from image processing will allow me to take battle UI elements and read values like health, MP and more.  This information
+will be critical to helping the AI model learn.
+
+Creting an interface for the AI model to use will allow me to train the AI while letting it interact with the game. 
+The interface creates "hotkey" functions I've defined to automate certain inputs (like moving around the menu to select a command).
+This way, the AI model can focus on battle strategy rather than figuring out how to input commands.
+
+The AI model will be the biggest and most complex part of the project.  Choosing which model and implementing it will be challenging.
+I will update the README with more information when I have more information on the AI model I'm using and my implementation.
--- typer.py
+++ typer.py
@@ -1,8 +1,88 @@
+#Luckman21
+'''
+FINAL FANTASY VII AI Battles
+
+Check out the README for more information
+'''
+
+import pytesseract
+import PIL.Image
+#import cv2
+import keyboard
 import pyautogui
 import time
+import random
 
-#Sleep for 5 seconds
+def Attack():
+    pyautogui.keyDown('i')
+    time.sleep(0.1)
+    pyautogui.keyUp('i')
+    time.sleep(0.1)
+    pyautogui.keyDown('i')
+    time.sleep(0.1)
+    pyautogui.keyUp('i')
+    time.sleep(0.1)
+
+def Magic():
+    pyautogui.keyDown('s')
+    time.sleep(0.1)
+    pyautogui.keyUp('s')
+    time.sleep(0.1)
+    Attack()
+
+def Summon():
+    pyautogui.keyDown('s')
+    time.sleep(0.1)
+    pyautogui.keyUp('s')
+    time.sleep(0.1)
+    Magic()
+
+def Item():
+    pyautogui.keyDown('s')
+    time.sleep(0.1)
+    pyautogui.keyUp('s')
+    time.sleep(0.1)
+    Summon()
+
+def eSkill():
+    pyautogui.keyDown('d')
+    time.sleep(0.1)
+    pyautogui.keyUp('d')
+    time.sleep(0.1)
+    Attack()
+
+#Sleep for 3 seconds
 time.sleep(3)
+
+while(keyboard.is_pressed('q') == False):
+
+    choice = random.randint(0,100)
+    time.sleep(3)
+
+    print(choice)
+    
+    if ((choice >= 0) & (choice <= 50)):
+        Attack()
+        print('Attack')
+
+    elif ((choice >= 51) & (choice <= 80)):
+        Magic()
+        print('Magic')
+
+    elif ((choice >= 81) & (choice <= 90)):
+        Summon()
+        print('Summon')
+
+    elif ((choice >= 91) & (choice <= 93)):
+        Item()
+        print('Item')
+
+    elif ((choice >= 94) & (choice <= 100)):
+        eSkill()
+        print('Enemy Skill')
+
+#Test code
+"""
 pyautogui.write('Hello World')
 
 txt = 'The quick brown fox jumps over the lazy dog'
@@ -10,3 +90,4 @@ txt = 'The quick brown fox jumps over the lazy dog'
 for i in txt:
     pyautogui.write(i)
     time.sleep(0.00005)
+"""
