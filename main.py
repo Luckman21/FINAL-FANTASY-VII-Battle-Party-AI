c49fb1d943aff580d41e68c3205be54e20fe9a76
Luckman21 <luckmaan21@gmail.com>
2023-11-13 21:46:13 -0500

    Main Control Script
    Created a main control script with a flow to allow the learning model's "training mode" to be toggled.  Also added a thread to create a KillSwitch when pressing the esc key.  This thread is a barebones implementation, but will make sure to save the current learning and halt keyboard inputs from the script before exiting all processes.

---
+++ main.py
@@ -0,0 +1,51 @@
+#Luckman21
+"""
+FINAL FANTASY VII AI Battles
+
+Check out the README for more information
+"""
+
+import os
+import threading
+import keyboard
+import time
+
+battle_toggle = False
+num = 0
+
+class KillSwitch(threading.Thread):
+    #Initialize the thread by overwriting the init method
+    def __init__(self, thread_name, thread_ID):
+        threading.Thread.__init__(self)
+        self.thread_name = thread_name #Defines the thread name
+        self.thread_ID = thread_ID #Defines the thread ID
+
+    #Overwrite the run method to define the code being run when started
+    def run(self):
+        while (keyboard.is_pressed('esc') == False):
+            continue
+        #Run shutdown sequence
+        print("--------Shutdown sequence initiated-------")
+        #Halt keyboard presses
+        print(" - Keyboard input halted")
+        #Save current learning model
+        print("- Learning model updated")
+        #quit()
+        os._exit(1)
+
+#Create thread and initialize it
+alive = KillSwitch("FFVII Battle AI", 1000)    
+alive.start()
+
+while (True):
+
+    if (keyboard.is_pressed('8')):
+        battle_toggle = True
+
+    while (battle_toggle):
+        #Run battle method
+        num += 1
+        print(num)
+
+        if (keyboard.is_pressed('9')):
+            battle_toggle = False
