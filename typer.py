b59eba5a639e7056d79eef2bc1e06f7630c8169f
Luckman21 <luckmaan21@gmail.com>
2024-05-04 02:54:56 -0400

    Test and Refactor Keyboard Macros
    Tested and refactored the keyboard macros.  Created "wasd" and OK macros to use for the more complex menu command macros.  Modified the macros to ensure they work when called from different files, since some lag caused consistent misinputs.

--- typer.py
+++ typer.py
@@ -7,53 +7,95 @@ Check out the README for more information
 import keyboard
 import pyautogui
 import time
+from datascrape import *
 from random import randint
 
-def Attack():
+def select():
+    """
+    Defines the "OK" button press.
+    """
     pyautogui.keyDown('i')
     time.sleep(0.1)
     pyautogui.keyUp('i')
     time.sleep(0.1)
-    pyautogui.keyDown('i')
-    time.sleep(0.1)
-    pyautogui.keyUp('i')
+
+def uArrow():
+    """
+    Defines the keypress for Up (W).
+    """
+    pyautogui.keyDown('w')
+    pyautogui.keyUp('w')
     time.sleep(0.1)
 
-def Magic():
+def dArrow():
+    """
+    Defines the keypress for Down (S).
+    """
     pyautogui.keyDown('s')
-    time.sleep(0.1)
     pyautogui.keyUp('s')
     time.sleep(0.1)
-    Attack()
 
-def Summon():
-    pyautogui.keyDown('s')
+def lArrow():
+    """
+    Defines the keypress for Left (A).
+    """
+    pyautogui.keyDown('a')
+    pyautogui.keyUp('a')
     time.sleep(0.1)
-    pyautogui.keyUp('s')
-    time.sleep(0.1)
-    Magic()
 
-def Item():
-    pyautogui.keyDown('s')
-    time.sleep(0.1)
-    pyautogui.keyUp('s')
-    time.sleep(0.1)
-    Summon()
-
-def eSkill():
+def rArrow():
+    """
+    Defines the keypress for Right (D).
+    """
     pyautogui.keyDown('d')
-    time.sleep(0.1)
     pyautogui.keyUp('d')
     time.sleep(0.1)
+
+def Attack():
+    """
+    Defines the macro for Attack.  Attack consists of 2 OK presses.
+    """
+    select()
+    select()
+
+def Magic(): #TODO: Select different magic spells.  Spells will be ordered depending on settings & materia levels.
+    """
+    Defines the macro for Magic.  Currently selects the topmost magic spell in the menu.
+    """
+    dArrow()
+    select()
+    Attack()
+
+def Summon(): #TODO: Select a summon based on the current equipped list.  Currently summons are organized in the menu by name, each has their own index position.
+    """
+    Defines the macro for Summon.  Currently selects the topmost Summon in the menu.
+    """
+    dArrow()
+    Magic()
+
+def Item(): #TODO: Select different items, give each an index ID to choose from the list.
+    """
+    Defines the macro for Item.  Currently selects the topmost Item in the menu.
+    """
+    dArrow()
+    Summon()
+
+def eSkill(): #TODO: Select different enemy skills, each is given a fixed index like summons.  Create a list to select the correct position.
+    """
+    Defines the macro for E.Skill.  Currently selects the topmost E.Skill in the menu.
+    """
+    rArrow()
+    select()
     Attack()
 
 #Sleep for 3 seconds
 time.sleep(3)
 
+'''
 while(keyboard.is_pressed('q') == False):
 
     choice = randint(0,100)
-    time.sleep(3)
+    time.sleep(5)
 
     print(choice)
     
@@ -76,7 +118,7 @@ while(keyboard.is_pressed('q') == False):
     elif ((choice >= 94) and (choice <= 100)):
         eSkill()
         print('Enemy Skill')
-
+'''
 #Test code
 """
 pyautogui.write('Hello World')
