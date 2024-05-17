39b8b25c50783fa65fa570bb1d4a11ca5b6c43d1
Luckman21 <luckmaan21@gmail.com>
2024-05-17 02:24:05 -0400

    Create testModule to for testing
    Created a testModule function in the main file to test features like reading HP values and correctly selecting menu items.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import os
 import threading
 import keyboard
 import time
+from random import randint
+from datascrape import *
+from typer import *
 
 battle_toggle = False
 num = 0
@@ -33,6 +36,56 @@ class KillSwitch(threading.Thread):
         #quit()
         os._exit(1)
 
+def testModule():
+    
+    pHP = HP()
+    enemyHP = eHP()
+
+    print("_" * 20)
+
+    for i in pHP:
+        if ((i[0] != 0) & (i[1] > 0)):
+            print(i[0] , "|" , i[1], round(i[0] * 100 / i[1]),"%")
+        elif ((i[0] == 0) & (i[1] > 0)):
+            print(i[0] , "|" , i[1], "0 %")
+
+    print("-" * 20)
+
+    for i in enemyHP:
+        if ((i[0] != 0) & (i[1] > 0)):
+            print(i[0] , "|" , i[1], round(i[0] * 100 / i[1]),"%")
+        elif ((i[0] == 0) & (i[1] > 0)):
+            print(i[0] , "|" , i[1], "0 %")
+
+    print("=" * 15)
+
+    if (((pHP[0][0] + pHP[1][0] + pHP[2][0]) != 0) & ((enemyHP[0][0] + enemyHP[1][0] + enemyHP[2][0] + enemyHP[3][0] + enemyHP[4][0] + enemyHP[5][0]) != 0)):
+        
+        if ((pHP[0][0] * 100 / pHP[0][1] <= 25) | (pHP[1][0] * 100 / pHP[1][1] <= 25) | (pHP[2][0] * 100 / pHP[2][1] <= 25)):
+            print("Item")
+            Item(2)
+        
+        else:
+            move = randint(0,9)
+            
+            if (move <= 3):
+                print("Attack")
+                Attack()
+            
+            elif (move == 4):
+                skill = randint(0, len(curr_eskill)-1)
+                print("Enemy Skill: "+ curr_eskill[skill])
+                eSkill(eskill.get(curr_eskill[skill]))
+            
+            elif (move == 5):
+                sum = randint(0, len(curr_summons)-1)
+                print("Summon: "+ curr_summons[sum])
+                Summon(int(summons.get(curr_summons[sum])))
+            
+            else:
+                print("Magic")
+                Magic()
+
 #Create thread and initialize it
 alive = KillSwitch("FFVII Battle AI", 1000)    
 alive.start()
@@ -44,8 +97,10 @@ while (True):
 
     while (battle_toggle):
         #Run battle method
-        num += 1
-        print(num)
+        testModule()
+        time.sleep(10)
+        #num += 1
+        #print(num)
 
         if (keyboard.is_pressed('9')):
             battle_toggle = False
