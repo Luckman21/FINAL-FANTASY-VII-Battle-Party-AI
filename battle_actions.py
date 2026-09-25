229cb6747174b98f364cc6beffd06ad1cb81ef3b
Luckman21 <luckmaan21@gmail.com>
2026-09-25 09:24:57 -0400

    Battle Actions Select Magic and E-Skill
    Added function fields to select specific magic spells, tiers and enemy skills.  Once materia is added to a character's object, we can better determine which spells and what tiers are available for each character.

--- battle_actions.py
+++ battle_actions.py
@@ -1,5 +1,6 @@
 #Luckman21
 from typer import *
+from game_info import summons, eskill
 
 def Attack():
     """
@@ -8,15 +9,24 @@ def Attack():
     select()
     select()
 
-def Magic(): #TODO: Select different magic spells.  Spells will be ordered depending on settings & materia levels.
+def Magic(spell, tier): #TODO: Select different magic spells.  Spells will be ordered depending on settings & materia levels.
     """
     Defines the macro for Magic.  Currently selects the topmost magic spell in the menu.
     """
     dArrow()
     select()
+
+    #Select spell
+    for i in range (0, spell):
+        dArrow()
+
+    #Select spell tier
+    for i in range (0, tier):
+        rArrow()
+
     Attack()
 
-def Summon(summon): #TODO: Select a summon based on the current equipped list.
+def Summon(summon):
     """
     Defines the macro for Summon.  Currently selects the topmost Summon in the menu.
     """
@@ -25,7 +35,7 @@ def Summon(summon): #TODO: Select a summon based on the current equipped list.
     select()
 
     #Scroll down until we reach the desired Summon
-    for i in range (0, summon):
+    for i in range(0, summons[summon]):
         dArrow()
     
     Attack()
@@ -35,9 +45,16 @@ def Item(item): #TODO: Update the item array so that items are listed in the cor
     Defines the macro for Item.  Selects an item based on the item's current specified index position.
     """
     dArrow()
-    Summon(item)
+    dArrow()
+    dArrow()
+    select()
 
-def eSkill(skill): #TODO: Create a list to select the correct position.
+    for i in range(0, item):    #TODO: Change to item_idx when the item index is created
+        dArrow()
+    
+    Attack()
+
+def eSkill(skill):
     """
     Defines the macro for E.Skill.  Selects an Enemy Skill based on the specified index position.
     """
@@ -45,11 +62,11 @@ def eSkill(skill): #TODO: Create a list to select the correct position.
     select()
     
     #The Enemy Skill menu is broken into 2 columns.  This logic will determine if the skill is found on the left or right column.
-    if (skill % 2 == 1):
+    if (eskill[skill] % 2 == 1):
         rArrow()
 
     #Scroll down until we reach the desired skill
-    for i in range (0, int(skill / 2)):
+    for i in range (0, int(eskill[skill] / 2)):
         dArrow()
     
     Attack()
