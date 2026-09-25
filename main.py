780ac25f987d61509c51f6f58b930ffbbb43712d
Luckman21 <luckmaan21@gmail.com>
2026-09-25 09:44:07 -0400

    Update Main V0.5
    Updated main.py to include battle_init which returns an instantiated BattleState object based on the new battle entered, as well as created an ATB observer thread to add characters to the ATB ready queue while work in the main battle loop is processed, preserving turn order.

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ import os
 import threading
 import keyboard
 import time
+from queue import Queue
+import game_state as g
 from random import randint
-from datascrape import *
 from typer import *
-from game_state import *
 
 battle_toggle = False
-num = 0
+atb_q = Queue(maxsize=3)    # Queue to keep track of turn order. Anyone in the queue is ready to initiate
+                            # an action.  First character is popped from the queue and has their actions executed
 
 class KillSwitch(threading.Thread):
     #Initialize the thread by overwriting the init method
@@ -37,59 +38,101 @@ class KillSwitch(threading.Thread):
         #quit()
         os._exit(1)
 
-def temp_battle_method():
-    move = randint(0,100)
+class ATB_observer(threading.Thread):
+    """
+    An observer thread that updates the ready ATB queue in real time so turn order
+    is preserved.
+    """
+    def __init__(self, thread_name:str, thread_ID:int, atb_q:Queue, battle:g.BattleState):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+        self.thread_name = thread_name
+        self.thread_ID = thread_ID
+        self.atb_q = atb_q
+        self.battle = battle
 
-    if (move >= 0 and move < 50):
-        Attack()
-    elif (move >= 50 and move < 75):
-        Magic()
-    elif (move >= 75 and move < 90):
-        Summon(10)
-    elif (move >= 90):
-        Item(0)
+    def run(self):
+        while not self.stop_event.is_set():
+            team = self.battle.party.get_party_members()
 
-def battle_status():
-    if (party_alive() and enemies_alive()):
-        print("Battle ongoing...")
-    elif (party_alive()):
-        print("Battle won!")
-    elif (enemies_alive()):
-        print("Game Over")
-    else:
-        print("Undefined state")
+            for player in team:
+                if (player.unit_ready() == True and atb_q.__contains__(player) == False):
+                    atb_q.put(player)
+                    
 
-def Player(name): #TODO: Implement
-    party_names.get(name)
+def battle_init():
+    """
+    Populates the battle functions based on whether units are active.  Inactive
+    units (not present on the field) are determined by a max hp = 0.  Only add
+    active units to the current battle.
+    """
 
-    #Remove party member from the queue
-    #Load character data
-    #Perform action
+    characters = [g.p1, g.p2, g.p3]
+    monsters = [g.e1, g.e2, g.e3, g.e4, g.e5, g.e6]
+    party_members = []
+    monster_team = []
+
+    for hero in characters:
+        if hero.unit_alive() == True:
+            party_members.append(hero)
+
+    for enemy in monsters:
+        if enemy.unit_alive() == True:
+            monster_team.append(enemy)
+
+    # PARTY DEFINITION
+    party = g.Party(
+        members = party_members
+    )
+
+    # ENEMIES DEFINITION
+    enemies = g.Monsters(
+        members = monster_team
+    )
+
+    # BATTLE STATE DEFINITION
+    battle = g.BattleState(
+        party = party,
+        enemies = enemies
+    )
+
+    return battle
+
+def battle_main(battle:g.BattleState):
+    """
+    Main battle control method.  Sets the flow of control for battle.
+
+    Until battle is finished, keep checking for actionable characters, pass the current
+    battle state to AI to determine the next move, execute it and repeat the process.
+    """
+    while (battle.party_alive() and battle.enemies_alive()):
+
+        # Spinlock for ATB queue to have an actionable party member
+        # the outer loop check added here ensures we don't get stuck forever
+        while (atb_q.empty() and battle.party_alive() and battle.enemies_alive()):
+            continue
+
+        player = atb_q.get()
+        # Pass player object to AI along with entire battle state
+        # Execute command for player
 
 #Create thread and initialize it
 alive = KillSwitch("FFVII Battle AI", 1000)    
 alive.start()
 
-#Creates the Queue for characters to take control of the controls
-atb_queue = []
-atb_queue_maxsize = 3
-
 while (True):
 
-    if (keyboard.is_pressed('8')):
+    if (keyboard.is_pressed('8')):  # Just kept for testing purposes, remove keyboard input battle toggle later
         battle_toggle = True
         #Update the party names in the dictionary
 
-    while (battle_toggle or (party_alive() and enemies_alive())):
-        #Run battle method
-        time.sleep(10)
-        temp_battle_method()
-        hp_stat_printer("Party HP", party_hp)
-        hp_stat_printer("Enemy HP", enemy_hp)
-        battle_status()
-        #num += 1
-        #print(num)
-        #party()
+        # Wait and detect battle start
+        # Pre-battle setup (spawn ATB thread)
+        battle_obj = battle_init()
+
+        # Main battle function
+        battle_main(battle_obj)
+        # Post-battle cleanup (kill ATB thread)    
 
         if (keyboard.is_pressed('9')):
             battle_toggle = False
